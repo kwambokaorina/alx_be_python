aa1e3c530032cba8ea1c9afe6d6c64fefb1bb8e5
Mitchel Kwamboka <130556636+kwambokaorina@users.noreply.github.com>
2024-10-13 01:44:00 -1200

    Update class_static_methods_demo.py

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,1 +1,15 @@
+class Calculator:
+    # Class attribute
+    calculation_type = "Arithmetic Operations"
+
+    # Static method
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    # Class method
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
 
